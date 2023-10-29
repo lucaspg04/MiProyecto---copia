d05759b3bc6eb45b9c830d53ba23aab74ffb93e2
merluu <ci.beyer@duocuc.cl>
2023-10-29 12:02:40 -0300

    se agega a firebase resgistrar usuario y modificar usuario

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router} from '@angular/router';
+import { FirebaseService } from '../services/firebase.service';
+import { User } from '../models/user.model';
+import { UtilsService } from '../services/utils.service';
+
 
 
 @Component({
@@ -11,21 +15,73 @@ import { Router} from '@angular/router';
 export class RegistroPage implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,private router: Router) { 
+  constructor(private formBuilder: FormBuilder, private router: Router) {
     this.loginForm = this.formBuilder.group({
-      user: ['', Validators.required],
+      name: ['', [Validators.required, Validators.minLength(4)]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(4)]], 
-    }); 
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
+
+  firebaseSvc = inject(FirebaseService);
+  utilsSvc = inject(UtilsService)
 
   ngOnInit() {
   }
 
-  onsubmit() {
+  //onsubmit() { // este es el original de aquí
+  //if (this.loginForm.valid) {
+  //Todos los campos están llenos, redirige a la página deseada
+  //this.router.navigate(['rol']);
+  //} else {
+  // Muestra un mensaje de error o realiza otra acción según tus necesidades
+  //console.log('Por favor, complete todos los campos.');
+  //}
+  //}
+
+
+  //async onsubmit() {
+  //if (this.loginForm.valid) {
+  //try {
+  //const user: User = this.loginForm.value as User;
+
+  // Registra al usuario
+  //await this.firebaseSvc.signUp( user);
+
+  // Actualiza el perfil del usuario
+  //await this.firebaseSvc.updateUser(user.name);
+
+  // Redirige al usuario a la página deseada después del registro
+  //this.router.navigate(['rol']);
+  //} catch (error) {
+  // Maneja errores aquí
+  //console.error(error);
+  //}
+  //} else {
+  // Muestra un mensaje de error o realiza otra acción según tus necesidades
+  //console.log('Por favor, complete todos los campos.');
+  //}
+  //}
+
+  async onsubmit() {
     if (this.loginForm.valid) {
-      // Todos los campos están llenos, redirige a la página deseada
-      this.router.navigate(['rol']);
+      try {
+        const user: User = this.loginForm.value as User;
+
+        // Registra al usuario
+        const signUpResult = await this.firebaseSvc.signUp(user);
+        console.log('Registro exitoso:', signUpResult);
+
+        // Actualiza el perfil del usuario
+        const updateResult = await this.firebaseSvc.updateUser(user.name);
+        console.log('Actualización del perfil exitosa:', updateResult);
+
+        // Redirige al usuario a la página deseada después del registro
+        //this.router.navigate(['rol']);
+      } catch (error) {
+        // Maneja errores aquí
+        console.error('Error durante el registro:', error);
+      }
     } else {
       // Muestra un mensaje de error o realiza otra acción según tus necesidades
       console.log('Por favor, complete todos los campos.');
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
-import {AngularFireAuth} from '@angular/fire/compat/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { UserCredential } from 'firebase/auth';
 import { User } from '../models/user.model';
 
 
@@ -8,16 +9,53 @@ import { User } from '../models/user.model';
 })
 export class FirebaseService {
 
-  constructor(private afAuth: AngularFireAuth) { }
+
+  constructor(private afAuth: AngularFireAuth) { 
+    
+  }
 
 
 
 
   //===== Autenticación======
 
+
+   //===== Acceder======
+
   signIn(user: User) {
     return this.afAuth.signInWithEmailAndPassword(user.email, user.password);
   }
+
+
+  signUp(user: User) {
+    return this.afAuth.createUserWithEmailAndPassword(user.email, user.password)
+      .then((userCredential) => {
+        // Actualizar el perfil del usuario con el nombre
+        return this.updateUser(user.name);
+      })
+      .catch((error) => {
+        // Manejar errores aquí
+        console.error(error);
+      });
+  }
+
+
+  updateUser(displayName: string) {
+    return this.afAuth.currentUser.then((user) => {
+      if (user) {
+        return user.updateProfile({ displayName });
+      } else {
+        // Manejar el caso en el que no haya un usuario autenticado
+        // Por ejemplo, mostrar un mensaje de error o redirigir al usuario a la página de inicio de sesión.
+        return Promise.reject("No hay un usuario autenticado.");
+      }
+    }).catch((error) => {
+      // Manejar errores aquí
+      console.error(error);
+      return Promise.reject(error);
+    });
+  }
+
 
 
 
